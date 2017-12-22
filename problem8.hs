666d4fe653109fedba48411d79c7420eca77e953
Nick L <5376991+nickoralias@users.noreply.github.com>
2017-12-21 23:31:13 -0800

    edit solution to problem 8 (solve without import statement)

--- problem8.hs
+++ problem8.hs
@@ -1,4 +1,8 @@
-import Data.List
-
 compress :: (Eq a) => [a] -> [a]
-compress = map head . group
+compress xs = compress' xs []
+    where
+        compress' [] _  = []
+        compress' [x] _ = [x]
+        compress' (x:xs) acc
+            | x == (head xs) = compress' xs acc
+            | otherwise    = x : compress' xs acc 
